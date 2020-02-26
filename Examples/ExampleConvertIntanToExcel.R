53473f0f8fece723dc063c1fa279f993217dce27
alicerueda <alicerueda@gmail.com>
2020-02-26 08:31:14 -0500

    Rename ExampleConvertIntanToExcel.R to Examples/ExampleConvertIntanToExcel.R

--- Examples/ExampleConvertIntanToExcel.R
+++ Examples/ExampleConvertIntanToExcel.R
@@ -5,5 +5,5 @@ source("read_Intan_RHD2000_file.R")
 
 # Convert all intan files in the folder where filename is located to an Excel file. 
 # File merging is executed so that split data (saved in different intan files) are put together
-filename <- file.choose() # Seleção do arquivo .rdh (intan)
+filename <- file.choose() # SeleÃ§Ã£o do arquivo .rdh (intan)
 ConvertRHD2Excel(filename)
